c84f5aaf99ba8e162f4a097dd53132c8d89f814c
Gavin Stark <gavin@gstark.com>
2016-12-21 10:35:49 -0500

    Adds database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+path = Rails.root.join("db/seeds/books", "*.json")
+Dir.glob(path) do |entry|
+  json = JSON.parse(File.read(entry))
+  authors = Array(json["authors"]).map { |author| Author.find_or_create_by(name: author) }
+  categories = Array(json["categories"]).map { |category| Category.find_or_create_by(name: category) }
+
+  Book.create(title: json["title"],
+              year: json["year"],
+              url: json["url"],
+              prices: json["prices"],
+              editor: json["editor"],
+              authors: authors,
+              categories: categories)
+end
