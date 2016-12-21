19a02edd9672fe44702a192120d2316e6900c58d
Gavin Stark <gavin@gstark.com>
2016-12-21 10:57:12 -0500

    Plain ruby filtering of authors

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,13 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all
+    author = Author.find_by(id: params["author"])
+
+    if author
+      @books = author.books
+    else
+      @books = Book.all
+    end
+
+    @authors = Author.all.order("name")
   end
 end
